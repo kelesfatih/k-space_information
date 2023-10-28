f7da53092b749517e83e8896a4c0fa9f958a3976
Fatih Keleş <fthkls270@gmail.com>
2023-10-29 00:17:27 +0300

    c5

---
+++ kSI.py
@@ -0,0 +1,10 @@
+from PIL import Image
+import numpy as np
+
+image = "test.tif"
+img_obj_gs = Image.open(image).convert("L")
+img_obj_gs_arr = np.array(img_obj_gs)
+
+# Parseval's Theorem Variance
+variance = np.sqrt((sum(np.square(img_obj_gs_arr.flatten() - np.mean(img_obj_gs_arr)))) /
+                   (2 * np.square(len(img_obj_gs_arr.flatten()))))
