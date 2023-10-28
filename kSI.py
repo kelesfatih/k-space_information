e6b3b6ac19abea991ad371244f135ee87b8536a5
Fatih Keleş <fthkls270@gmail.com>
2023-10-29 00:29:31 +0300

    c5

--- kSI.py
+++ kSI.py
@@ -8,3 +8,8 @@ img_obj_gs_arr = np.array(img_obj_gs)
 # Parseval's Theorem Variance
 variance = np.sqrt((sum(np.square(img_obj_gs_arr.flatten() - np.mean(img_obj_gs_arr)))) /
                    (2 * np.square(len(img_obj_gs_arr.flatten()))))
+# HkS
+
+# IkS
+
+# kSI
